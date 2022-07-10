e591623cde21eabfe43df620c8f6ffa893acd083
umbreon99 <pel.kensei43@gmail.com>
2022-07-10 15:45:04 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        fSchema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
 
         if (\App::environment(['production'])) {
             \URL::forceScheme('https');
